ae85d8eb0b21bd586b12cbef78090cb6c2c6229a
gabrielleacuba <gabrielleacuba@gmail.com>
2024-05-28 19:53:19 -0300

    feat(cypress): add cypress and create e2e test

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress'
+import { on } from 'events'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
